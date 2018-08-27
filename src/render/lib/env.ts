251ecc86a9fe1c26423760f9d9e3ce73976c86f0
Xiao <xiaoboost@163.com>
2018-08-28 00:16:41 +0800

    fix tsconfig

---
+++ src/render/lib/env.ts
@@ -0,0 +1,8 @@
+import { resolve } from 'path';
+
+export const appRoot = resolve(
+    __dirname,
+    process.env.NODE_ENV === 'development'
+        ? '../../../../../../dist'
+        : '../../..',
+);
