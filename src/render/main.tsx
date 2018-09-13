75e9f3812fd6b43be4a72fc2cf146609b48e503e
Xiao <xiaoboost@163.com>
2018-09-13 23:06:20 +0800

    delete tag list

--- src/render/main.tsx
+++ src/render/main.tsx
@@ -1,13 +1,13 @@
 import './css/main';
 
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import MainList from 'views/main-list/component';
 import ItemDetail from 'views/item-detail/component';
 
-ReactDOM.render(
+render(
     <HashRouter>
         <Switch>
             <Route exact path="/" component={MainList} />
