bf34393715bae3fc3fd5fb2464b74e3127a2fb3f
Xiao <xiaoboost@163.com>
2018-09-14 23:39:59 +0800

    finish zip lib

--- src/render/lib/error.ts
+++ src/render/lib/error.ts
@@ -1,7 +1,12 @@
 import Message from 'antd/lib/message';
 
 export function handleError(error: Error, info?: string) {
-    const text = info ? info : error.message;
+    const text = info
+        ? process.env.NODE_ENV === 'development'
+            ? `${info}\n${error.message}`
+            : info
+        : error.message;
+
     Message.error(text);
     throw error;
 }
--- src/render/lib/zip.ts
+++ src/render/lib/zip.ts
@@ -1,10 +1,9 @@
 import fs from 'fs-extra';
+import path from 'path';
 import JSZip from 'jszip';
-import { sep, join, relative } from 'path';
 
 import { handleError } from './error';
-
-const regExpSep = new RegExp(sep, 'g');
+import { isString, isArray } from './assert';
 
 /** 读取文件夹内所有文件的路径 */
 async function readDir(dir: string) {
@@ -16,7 +15,7 @@ async function readDir(dir: string) {
         const files = await fs.readdir(dir);
 
         for (const file of files) {
-            result.push(...await readDir(join(dir, file)))
+            result.push(...await readDir(path.join(dir, file)))
         }
     }
     // 该路径是文件
@@ -30,19 +29,43 @@ async function readDir(dir: string) {
 export default class Zip {
     /** 读取 zip 文件 */
     static async loadZip(zipPath: string) {
+        const zip = new Zip(path.parse(zipPath).name);
+        const zipContent = await fs.readFile(zipPath);
 
+        zip._zip.loadAsync(zipContent);
+
+        return zip;
     }
     /** 读取需要压缩的文件 */
     static async loadFiles(filesPath: string | string[]) {
-        const zipper = new Zip();
+        let currentDir: string;
+
+        if (isString(filesPath)) {
+            const stat = await fs.stat(filesPath);
+            if (stat.isDirectory()) {
+                currentDir = filesPath;
+            }
+            else {
+                currentDir = path.resolve(filesPath, '..');
+            }
+        }
+        else {
+            currentDir = path.resolve(filesPath[0], '..');
+        }
+
+        const zipper = new Zip(path.parse(currentDir).name);
+
         await zipper.append(filesPath);
         return zipper;
     }
 
     /** 压缩包数据本体 */
     private _zip: JSZip;
+    /** 压缩包名称 */
+    name: string;
 
-    constructor() {
+    constructor(name: string) {
+        this.name = name;
         this._zip = new JSZip();
     }
 
@@ -60,24 +83,71 @@ export default class Zip {
         
         for (const filePath of files) {
             const content = await fs.readFile(filePath);
-            const path = basePath ? relative(basePath, filePath) : filePath;
+            const pathInZip = path.normalize(
+                basePath
+                    ? path.relative(basePath, filePath)
+                    : filePath
+            );
             
-            this._zip.file(path.replace(regExpSep, '/'), content);
+            this._zip.file(pathInZip, content);
         }
     }
     /** 从压缩包中移除文件 */
-    async remove(files: string | string[]) {
+    remove(files: string | string[]) {
+        const matchs = isArray(files) ? files : [files];
 
-        return this;
+        for (const match of matchs) {
+            this._zip.remove(match);
+        }
     }
     /** 解压缩当前压缩包 */
     async unPack(targetDir: string) {
+        const errorFiles: string[] = [];
 
-        return this;
+        for (const [innerPath, data] of Object.entries(this._zip.files)) {
+            if (data.dir) {
+                continue;
+            }
+
+            const filePath = path.join(targetDir, innerPath);
+            const { dir: fileDir } = path.parse(filePath);
+
+            await (new Promise((resolve) => {
+                fs.mkdirp(fileDir)
+                    .then(() => this._zip
+                        .file(innerPath)
+                        .nodeStream()
+                        .pipe(fs.createWriteStream(filePath))
+                        .on('finish', resolve)
+                        .on('error', () => {
+                            errorFiles.push(innerPath);
+                            resolve();
+                        })
+                    )
+                    .catch(() => {
+                        errorFiles.push(innerPath);
+                        resolve();
+                    })
+            }));
+        }
     }
     /** 将压缩包写入硬盘 */
-    async write(targetDir: string) {
+    write(targetDir: string): Promise<boolean> {
+        const targetFile = path.join(targetDir, `${this.name}.zip`);
 
-        return this;
+        return new Promise((resolve) => {
+            this._zip
+                .generateNodeStream({
+                    type: 'nodebuffer',
+                    compression: 'STORE',
+                    streamFiles: true,
+                })
+                .pipe(fs.createWriteStream(targetFile))
+                .on('finish', () => resolve(true))
+                .on('error', (err: Error) => {
+                    handleError(err, `Can not write this file: ${targetFile}`);
+                    resolve(false);
+                });
+        });
     }
 }
