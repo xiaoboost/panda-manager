c1822b1bc96821804e25805756a99f3146c46ec6
xiaoboost <xiaoboost@qq.com>
2018-10-01 10:02:08 +0800

    delete main-list Header

--- src/render/views/main-list/component.tsx
+++ src/render/views/main-list/component.tsx
@@ -1,13 +1,21 @@
 import './component.styl';
+import 'antd/lib/button/style'
 
 import * as React from 'react';
-
-import Header from './components/header/component';
+import Button from 'antd/lib/button';
+import { selectDirectory } from 'lib/utils';
 
 export default class MainList extends React.Component {
+    addFolder = async () => {
+        const directories = await selectDirectory();
+        console.log(directories);
+    }
+
     render() {
         return <div id="main-list">
-            <Header></Header>
+            <header className="main-list-header">
+                <Button onClick={this.addFolder}>添加文件夹</Button>
+            </header>
             缩略图列表
         </div>;
     }
