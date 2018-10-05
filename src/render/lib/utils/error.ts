d42d50e6e96f44f9228e7ef7a55fe3ea8684f311
xiaoboost <xiaoboost@qq.com>
2018-10-05 19:55:41 +0800

    set error code

--- src/render/lib/utils/error.ts
+++ src/render/lib/utils/error.ts
@@ -1,13 +1,22 @@
 import 'antd/lib/message/style';
 import Message from 'antd/lib/message';
 
-export function handleError(error: Error, info?: string) {
-    const text = info
-        ? process.env.NODE_ENV === 'development'
-            ? `${info}\n${error.message}`
-            : info
-        : error.message;
+/** 错误码 */
+const errorCode = {
+    // 100 ~ 199 文件与文件夹
+    100: '文件夹不存在',
+    101: '不允许重复添加文件夹',
+    102: '只允许刷新已经添加的文件夹',
+    103: '此漫画已经被删除，它将会被移出列表',
 
-    Message.error(text);
-    throw error;
+    // 200 ~ 299
+    200: '压缩包损坏',
+};
+
+export function handleError(code: keyof typeof errorCode, info?: string) {
+    Message.error(
+        info
+            ? errorCode[code]
+            : `${errorCode[code]}：\n ${info}`
+    );
 }
