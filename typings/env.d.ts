3c7c8c3eeef24db9cc552ea0027284303dcc2d76
Xiao <xiaoboost@qq.com>
2019-08-04 19:33:23 +0800

    electron env file defined

---
+++ typings/env.d.ts
@@ -0,0 +1,9 @@
+/** 全局环境变量补充 */
+declare namespace NodeJS {
+    interface ProcessEnv {
+        /** 编译模式 */
+        NODE_ENV: 'development' | 'production';
+        /** 初始目录 */
+        INIT_CWD: string;
+    }
+}
