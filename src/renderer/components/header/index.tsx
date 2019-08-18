b75579eb3f66daa7599b06d6ea7333cad7fe1567
Xiao <xiaoboost@qq.com>
2019-08-18 23:04:34 +0800

    new global header init

---
+++ src/renderer/components/header/index.tsx
@@ -0,0 +1,58 @@
+import './index.styl';
+
+import { remote } from 'electron';
+import { default as React, useState, useEffect } from 'react';
+
+import Icon from 'antd/es/icon';
+
+function useIsMaximize() {
+    const win = remote.getCurrentWindow();
+    const [isMaximize, setState] = useState(win.isMaximized());
+
+    useEffect(() => {
+        const setMaximize = () => setState(true);
+        const setUnMaximize = () => setState(false);
+
+        win.on('maximize', setMaximize);
+        win.on('unmaximize', setUnMaximize);
+
+        return () => {
+            win.removeListener('maximize', setMaximize);
+            win.removeListener('unmaximize', setUnMaximize);
+        };
+    }, []);
+
+    return isMaximize;
+}
+
+const Recover = () => (
+    <i className="anticon app-title-bar__icon">
+        <svg viewBox="64 64 1024 1024" version="1.1" fill="currentColor" aria-hidden="true" width="1em" height="1em">
+            <path d="M890.41032533 75.09333333h-573.472768c-32.555008 0-59.04247467 26.279936-59.04247466 58.5728V255.91808H134.68194133c-32.56046933 0-59.04247467 26.279936-59.04247466 58.57826133v575.832064c0 32.29832533 26.48200533 58.57826133 59.04247466 58.57826134H708.149248c32.54954667 0 59.04247467-26.279936 59.04247467-58.57826134V768.07645867h123.21860266c32.555008 0 59.04247467-26.27447467 59.04247467-58.57826134v-575.832064c0-32.292864-26.48746667-58.5728-59.04247467-58.5728z m-188.82013866 808.72516267H141.24100267V321.00078933h560.349184V883.818496zM883.851264 702.99374933H767.19172267V314.49634133c0-32.29832533-26.492928-58.57826133-59.04247467-58.57826133H323.50208V140.17604267H883.851264V702.99374933z"></path>
+        </svg>
+    </i>
+);
+
+export default function Header() {
+    const isMaximize = useIsMaximize();
+
+    return (
+        <header className='app-header'>
+            <span>
+                <i className='app-title-bar__icon'></i>
+                <span className='app-title-bar__title'>Panda Manager</span>
+            </span>
+            <span>
+                {/* 最小化 */}
+                <Icon className='app-title-bar__icon' type="minus" />
+                {isMaximize
+                    /* 还原 */
+                    ? <Recover />
+                    /* 最大化 */
+                    : <Icon className='app-title-bar__icon' type="border" />}
+                {/* 关闭 */}
+                <Icon className='app-title-bar__icon icon-close' type="close" />
+            </span>
+        </header>
+    );
+}
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -3,21 +3,25 @@ import './css/main';
 import * as React from 'react';
 
 import { render } from 'react-dom';
+
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
-import Setting from 'renderer/views/setting';
-import MangaList from 'renderer/views/manga-list';
-import ItemDetail from 'renderer/views/manga-detail';
-import TagCollection from 'renderer/views/tag-collection';
+// import Setting from 'renderer/views/setting';
+// import MangaList from 'renderer/views/manga-list';
+// import ItemDetail from 'renderer/views/manga-detail';
+// import TagCollection from 'renderer/views/tag-collection';
+
+import Header from 'src/renderer/components/header';
 
 render(
     <HashRouter>
-        <Switch>
+        <Header />
+        {/* <Switch>
             <Route exact path='/' component={MangaList} />
             <Route path='/setting' component={Setting} />
             <Route path='/tags' component={TagCollection} />
             <Route path='/detail/:id' component={ItemDetail} />
-        </Switch>
+        </Switch> */}
     </HashRouter>,
     document.getElementById('app')!,
 );
