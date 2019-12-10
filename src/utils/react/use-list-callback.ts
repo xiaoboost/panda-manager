ef3cd4f46823bd56fac78ef47f719d35a84f1a07
xiaoboost <xiaoboost@qq.com>
2019-12-10 17:23:19 +0800

    fix list callback

--- src/utils/react/use-list-callback.ts
+++ src/utils/react/use-list-callback.ts
@@ -9,7 +9,7 @@ export default function useListCallback<T, F extends AnyFunction>(data: T[], cre
     /** 上一次的原始数据 */
     const ref = useRef(new Map<T, F>());
     /** 当前缓存记录 */
-    const map = new Map(new Map<T, F>());
+    const map = new Map<T, F>();
 
     useEffect(() => {
         ref.current = map;
