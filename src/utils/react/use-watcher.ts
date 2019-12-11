a502a239a6d9c567f8c91d2641c54ecd4742dcc6
xiaoboost <xiaoboost@qq.com>
2019-12-11 22:39:15 +0800

    fix use-watcher

--- src/utils/react/use-watcher.ts
+++ src/utils/react/use-watcher.ts
@@ -1,21 +1,31 @@
 import useForceUpdate from './use-force-update';
 
-import { Watcher } from '../shared/subject';
 import { useRef, useEffect } from 'react';
+import { Watcher, BaseType, isBaseType } from '../shared';
 
+export default function useWatcher<T extends BaseType>(watcher: Watcher<T>): [T, (val: T) => void];
+export default function useWatcher<T extends object>(watcher: Watcher<T>): [Readonly<T>, (val: Partial<T>) => void];
 export default function useWatcher<T>(watcher: Watcher<T>) {
     const update = useForceUpdate();
     const state = useRef(watcher.data);
 
-    function handleStatusChange(val: T) {
-        state.current = val as any;
-        update();
-    }
+    const handleChange = isBaseType(watcher.data)
+        ? (val: T) => {
+            state.current = val as any;
+            update();
+        }
+        : (val: Partial<T>) => {
+            state.current = {
+                ...state.current,
+                ...val,
+            };
+            update();
+        };
 
     useEffect(() => {
-        watcher.observe(handleStatusChange);
-        return () => watcher.unObserve(handleStatusChange);
+        watcher.observe(handleChange);
+        return () => watcher.unObserve(handleChange);
     }, []);
 
-    return [state.current, handleStatusChange] as const;
+    return [state.current, handleChange] as const;
 }
