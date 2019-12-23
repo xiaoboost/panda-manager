036a845910adecba59d5acd917b8b2f8b49ee308
xiaoboost <xiaoboost@qq.com>
2019-12-23 17:49:49 +0800

    safe sandbox init

--- packages/module-controller/src/index.ts
+++ packages/module-controller/src/index.ts
@@ -1,11 +1,26 @@
+import { runInNewContext } from 'vm';
 import { use } from './utils';
 
 export * from './types';
 
 export { getModule } from './utils';
 
+const ModuleContext = runInNewContext('(Function)', Object.assign(Object.create(null), {
+    modules: {
+        exports: {},
+    },
+}));
+
 export async function createMeta(path: string) {
     // ..
+}
+
+function getModules(code: string) {
+    const subModule = new ModuleContext(
+        `${code}; return modules.exports;`,
+    )();
+
+    return subModule;
 }
 
 export const ready = (() => {
