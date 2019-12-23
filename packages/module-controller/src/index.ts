f397bd33f40d5780f25c80ab284f7b8c86fa75dd
xiaoboost <xiaoboost@qq.com>
2019-12-23 11:19:54 +0800

    add method

--- packages/module-controller/src/index.ts
+++ packages/module-controller/src/index.ts
@@ -1,6 +1,12 @@
 import { use } from './utils';
 
+export * from './types';
+
 export { getModule } from './utils';
+
+export async function createMeta(path: string) {
+    // ..
+}
 
 export const ready = (() => {
     return Promise.resolve();
