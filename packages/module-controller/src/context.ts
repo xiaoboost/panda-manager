933eb6128b23bbb0dfbc12901373b72952d0ad15
xiaoboost <xiaoboost@qq.com>
2020-01-01 14:33:05 +0800

    context init

---
+++ packages/module-controller/src/context.ts
@@ -0,0 +1,10 @@
+import * as fs from '@utils/node/file-system';
+import * as image from '@utils/node/image';
+
+import antd from 'antd';
+
+export {
+    fs,
+    image,
+    antd,
+};
--- packages/module-controller/src/index.ts
+++ packages/module-controller/src/index.ts
@@ -1,28 +1,64 @@
-import { runInNewContext } from 'vm';
-import { use } from './utils';
+import { VM } from 'vm2';
+import { Module } from './types';
+
+import { resolveUserDir } from '@utils/shared';
+
+import * as ModuleContext from './context';
+import * as fs from '@utils/node/file-system';
 
 export * from './types';
-
 export { getModule } from './utils';
 
-const ModuleContext = runInNewContext('(Function)', Object.assign(Object.create(null), {
-    modules: {
-        exports: {},
-    },
-}));
+/** 扩展的储存路径 */
+const extensionPath = resolveUserDir('extensions');
+/** 扩展列表 */
+const extensions: Module[] = [];
 
+/** 创建项目元数据 */
 export async function createMeta(path: string) {
     // ..
 }
 
-function getModules(code: string) {
-    const subModule = new ModuleContext(
-        `${code}; return modules.exports;`,
-    )();
+function createModule(code: string) {
+    /** 模块沙盒 */
+    const vm = new VM({
+        sandbox: {
+            ...ModuleContext,
+            modules: {
+                exports: {},
+            },
+        },
+    });
 
-    return subModule;
+    const subModule = vm.run(
+        `(function getModule() {
+            ${code};
+            return modules.exports;
+        })()`,
+    );
+
+    return subModule as Module;
 }
 
-export const ready = (() => {
+export const ready = (async () => {
+    // 扩展文件夹不存在
+    if (!fs.existsSync(extensionPath)) {
+        return fs.mkdirp(extensionPath);
+    }
+
+    const files = await fs.readdir(extensionPath);
+
+    for (let i = 0; i < files.length; i++) {
+        const content = await fs.readFile(files[i]).catch(() => void 0);
+
+        if (!content) {
+            continue;
+        }
+
+        const module = createModule(content.toString());
+
+        extensions.push(module);
+    }
+
     return Promise.resolve();
 })();
