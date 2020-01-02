cbd253b5a851cc2b8d2e5fecf666789df68413a1
xiaoboost <xiaoboost@qq.com>
2020-01-03 01:54:39 +0800

    fix native module

--- packages/utils/src/shared/console.ts
+++ packages/utils/src/shared/console.ts
@@ -1,7 +1,8 @@
 if (console) {
-    const originConsole = console;
+    const originConsole: typeof console = {} as any;
 
-    Object.keys(originConsole).forEach((key) => {
+    Object.keys(console).forEach((key) => {
+        originConsole[key] = console[key];
         console[key] = (...args: any[]) => {
             if (process.env.NODE_ENV === 'development') {
                 originConsole[key](...args);
--- packages/utils/src/shared/native.ts
+++ packages/utils/src/shared/native.ts
@@ -1,18 +1,20 @@
 /* eslint-disable no-eval */
 
 // 原生 eval 函数重命名
-export const _eval: typeof eval = (window as any).eval;
+export const _eval: typeof eval = globalThis.eval;
 
 // 原生 write 函数重命名
-export const _write = document.write;
+export const _write = globalThis.document ? globalThis.document.write : () => void 0;
 
 // disabled dangerous api
 if (process.env.NODE_ENV !== 'development') {
-    (window as any).eval = global.eval = () => {
+    globalThis.eval = () => {
         throw new Error('Sorry, this app does not support window.eval().');
     };
 
-    document.write = () => {
-        throw new Error('Sorry, this app does not support document.write().');
-    };
+    if (globalThis.document) {
+        globalThis.document.write = () => {
+            throw new Error('Sorry, this app does not support document.write().');
+        };
+    }
 }
