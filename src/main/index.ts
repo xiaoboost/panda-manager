33266573d03b316f163addce86913e70c44cf6d1
xiaoboost <xiaoboost@qq.com>
2020-07-04 14:47:35 +0800

    remove temp files when close app

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,9 @@ import { app } from 'electron';
 import { win, install as installRenderer } from './main';
 import { install as installServer } from 'src/server/main';
 
+import { rmrf } from 'src/utils/node/file-system';
+import { resolveTempDir } from 'src/utils/node/env';
+
 // 完成初始化时启动主界面
 app.on('ready', () => {
     installRenderer();
@@ -12,6 +15,11 @@ app.on('ready', () => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
+    }
+
+    // 清理临时文件夹
+    if (process.env.NODE_ENV === 'production') {
+        rmrf(resolveTempDir());
     }
 });
 
