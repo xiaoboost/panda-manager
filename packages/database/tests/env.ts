7b439048c101c8b35c9464734eaf378d18cfb9a9
xiaoboost <xiaoboost@qq.com>
2021-02-22 00:26:37 +0800

    test init

--- packages/database/tests/env.ts
+++ packages/database/tests/env.ts
@@ -1,15 +1,16 @@
 import Module from 'module';
 
-const originalRequire = Module.prototype.require;
+export function install() {
+  const originalRequire = Module.prototype.require;
 
-(Module.prototype as any).require = function require(this: Module, path: string) {
-  if (path === 'fs') {
-    return originalRequire.call(this, 'memfs');
-  }
-  else {
-    return originalRequire.call(this, path);
-  }
-};
+  (Module.prototype as any).require = function require(this: Module, path: string) {
+    if (path === 'fs') {
+      return originalRequire.call(this, 'memfs');
+    }
+    else {
+      return originalRequire.call(this, path);
+    }
+  };
 
-// 测试模式
-process.env.NODE_ENV = 'development';
+  (process.env as any).NODE_ENV = 'test';
+}
